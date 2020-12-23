e4240b821839f1c18d2a7983bd42868c16f68025
Bernhard Richter <bernhard.richter@gmail.com>
2020-12-23 15:31:36 +0100

    Fixed build config

--- src/LightMock/ExpressionReflect/TypeExtensions.cs
+++ src/LightMock/ExpressionReflect/TypeExtensions.cs
@@ -1,57 +1,59 @@
 ﻿namespace ExpressionReflect
 {
-	using System;
+    using System;
+    using System.Diagnostics.CodeAnalysis;
     using System.Reflection;
 
+    [ExcludeFromCodeCoverage]
     internal static class TypeExtensions
-	{
-		internal static bool IsFunc(this Type type)
-		{
-			bool isFunc = false;
+    {
+        internal static bool IsFunc(this Type type)
+        {
+            bool isFunc = false;
 
-			if (type.GetTypeInfo().IsGenericType)
-			{
-				Type definition = type.GetGenericTypeDefinition();
-				isFunc = definition == typeof(Func<>) ||
-					definition == typeof(Func<,>) ||
-					definition == typeof(Func<,,>) ||
-					definition == typeof(Func<,,,>) ||
-					definition == typeof(Func<,,,,>);
-			}
+            if (type.GetTypeInfo().IsGenericType)
+            {
+                Type definition = type.GetGenericTypeDefinition();
+                isFunc = definition == typeof(Func<>) ||
+                    definition == typeof(Func<,>) ||
+                    definition == typeof(Func<,,>) ||
+                    definition == typeof(Func<,,,>) ||
+                    definition == typeof(Func<,,,,>);
+            }
 
-			return isFunc;
-		}
+            return isFunc;
+        }
 
-		internal static bool IsAction(this Type type)
-		{
-			bool isAction = type == typeof(Action);
+        internal static bool IsAction(this Type type)
+        {
+            bool isAction = type == typeof(Action);
 
-			if (type.GetTypeInfo().IsGenericType)
-			{
-				Type definition = type.GetGenericTypeDefinition();
+            if (type.GetTypeInfo().IsGenericType)
+            {
+                Type definition = type.GetGenericTypeDefinition();
 
-				isAction = isAction ||
-					definition == typeof(Action<>) ||
-					definition == typeof(Action<,>) ||
-					definition == typeof(Action<,,>) ||
-					definition == typeof(Action<,,,>);
-			}
+                isAction = isAction ||
+                    definition == typeof(Action<>) ||
+                    definition == typeof(Action<,>) ||
+                    definition == typeof(Action<,,>) ||
+                    definition == typeof(Action<,,,>);
+            }
 
-			return isAction;
-		}
+            return isAction;
+        }
 
-		internal static bool IsPredicate(this Type type)
-		{
-			bool isPredicate = false;
+        internal static bool IsPredicate(this Type type)
+        {
+            bool isPredicate = false;
 
-			if (type.GetTypeInfo().IsGenericType)
-			{
-				Type definition = type.GetGenericTypeDefinition();
-				isPredicate = definition == typeof(Predicate<>);
-			}
+            if (type.GetTypeInfo().IsGenericType)
+            {
+                Type definition = type.GetGenericTypeDefinition();
+                isPredicate = definition == typeof(Predicate<>);
+            }
 
-			return isPredicate;
-		}
+            return isPredicate;
+        }
 
         internal static bool IsInstanceOfType(this Type type, object obj)
         {
